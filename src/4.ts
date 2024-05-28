b330d1c51c856964f1623e8774fd0ba7c9df25e4
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-05-28 21:39:21 +0300

    home work 4

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,40 @@
-const key = new Key();
+class Key {
+    constructor(private key: string){}
+
+    getKey(): string{
+        return this.key
+    }
+}
+
+class MyHouse {
+    constructor (private key: Key){ }
+
+    openDoor(value: string) {
+        if(value === this.key.getKey()) {
+            console.log('Dor is open')
+        } else {
+            console.log('Key is wrong')
+        }
+    }
+
+    comeIn(person: Person) {
+        const userKey = person.getKey()
+        if(userKey === this.key.getKey()){
+            console.log('Come in')
+        } else {
+            console.log('Bye')
+        }
+    }
+}
+
+class Person {
+    constructor(private key: Key) { }
+    getKey() {
+        return this.key.getKey()
+    }
+}
+
+const key = new Key('KEY');
 
 const house = new MyHouse(key);
 const person = new Person(key);
