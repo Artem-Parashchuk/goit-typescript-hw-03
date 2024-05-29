b6e0fddfa8075c53f077f2e287ff6c61d7e5b6f5
Artem Parashchuk <a.p.paraschuk@gmail.com>
2024-05-29 20:33:36 +0300

    fix class House and MyHouse

--- src/4.ts
+++ src/4.ts
@@ -1,61 +1,53 @@
 class Key {
-    private signature: number;
-  
-    constructor(signature: number) {
-      this.signature = signature !== undefined ? signature : Math.random();
-    }
-  
-    public getSignature(): number {
-      return this.signature;
-    }
+  private signature: number;
+
+  constructor(signature: number) {
+    this.signature = signature !== undefined ? signature : Math.random();
   }
 
-class Person {
-    constructor(private key: Key) { }
+  public getSignature(): number {
+    return this.signature;
+  }
+}
 
-    getKey(): Key {
-        return this.key;
-    }
+class Person {
+  constructor(private key: Key) { }
+
+  getKey(): Key {
+    return this.key;
+  }
 }
 
 abstract class House {
-    protected door: boolean;
-    protected key: Key;
-    protected tenants: Person[];
-  
-    constructor(key: Key) {
-      this.door = false; // Двері закриті за замовчуванням
-      this.key = key;
-      this.tenants = [];
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Open!');
+    } else {
+      console.log("Door is closed!");
     }
-  
-    comeIn(person: Person): void {
-      if (this.door) {
-        this.tenants.push(person);
-        console.log('Open!');
-      } else {
-        console.log("Door is closed!");
-      }
-    }
-  
-    abstract openDoor(key: Key): void;
   }
 
+  abstract openDoor(key: Key): void;
+}
 
-  class MyHouse extends House {
-    constructor(key: Key) {
-      super(key);
-    }
-  
-    openDoor(key: Key): void {
-      if (key.getSignature() === this.key.getSignature()) {
-        this.door = true;
-        console.log("Door is opened successfully.");
-      } else {
-        console.log("Dor is closed");
-      }
+
+class MyHouse extends House {
+
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log("Door is opened successfully.");
+    } else {
+      console.log("Dor is closed");
     }
   }
+}
 
 const key = new Key(12);
 
